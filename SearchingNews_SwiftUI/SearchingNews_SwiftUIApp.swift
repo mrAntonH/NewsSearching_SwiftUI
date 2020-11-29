d5ec50b565bec82021362fcffba6d1ded1b54b42
Anton <antonsvec@MacBook-Pro-User.local>
2020-11-29 22:01:34 +0400

    initial commit

---
+++ SearchingNews_SwiftUI/SearchingNews_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchingNews_SwiftUIApp.swift
+//  SearchingNews_SwiftUI
+//
+//  Created by Антон Швец on 29.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct SearchingNews_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
